bb04e807afd42ddd34873c4c1503dec3471336ab
rizmimaitri <rizmimn@gmail.com>
2025-07-15 13:50:37 +0700

    Update streamlit_app.py
    Signed-off-by: rizmimaitri <rizmimn@gmail.com>

--- streamlit_app.py
+++ streamlit_app.py
@@ -93,8 +93,8 @@ soal_data = {
         {"question": "?", "options": ["", "", "",""], "answer": ""},
         {"question": "?", "options": ["", "", "",""], "answer": ""},
         {"question": "?", "options": ["", "", "",""], "answer": ""},  
-        
 }
+
 
 # Halaman Beranda
 if menu == "Beranda":
